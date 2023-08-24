d29d0740938434b5facdd639aafbea00bb62cc7f
TLHargreaves <hteyanna1@gmail.com>
2023-08-24 05:57:34 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - prints all alphabets in lowercase
- * Return: Always 0
+ * Return: On success 1
  */
 void print_alphabet(void)
 {
@@ -13,5 +13,4 @@ void print_alphabet(void)
 	_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
